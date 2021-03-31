e5e9b6f3fb4087454d817d6ae5fbb51b626f92f0
deyan petrov <deyanp19@hotmail.com>
2021-03-31 10:18:11 +0300

    footer css

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,8 +5,16 @@ function Body() {
                    <h3>
                        The Fighter Jets in the skys
                    </h3>
-        <p>The intention of the page is to provide classification of the fighter jets in use. The engines and specifications each class and model of fighter jets have. The maim use of fighter jets is air superiority and the advancement of the capabilities are claiisfing them in <i><b>generations.</b></i> </p>
-        <p>Currently fighter Jets are up to 6-th generation. With the only one pretender that have reported flight is flown in <a href="https://nationalinterest.org/blog/reboot/americas-6th-generation-stealth-fighter-could-be-everything-164442">USA more here.</a>  </p>
+        <p>The intention of the page is to provide classification of the fighter jets in use. The engines and specifications each class and model of fighter jets have. The maim use of fighter jets is air superiority and the advancement of the capabilities are claisfing them in <i><b>generations.</b></i> </p>
+        <p>Currently the top fighter Jets tier are the 6-th generation. With the only one pretender that have reported flight the USA is leading force. It has been reported by officials that it has been  flown in USA in s secret location in 2020.<a href="https://nationalinterest.org/blog/reboot/americas-6th-generation-stealth-fighter-could-be-everything-164442"> More here...</a> 
+        <p>
+            <ul>
+                <li><a href="https://www.airforce-technology.com/features/top-sixth-generation-fighter-jets/">Airforce Technology Website</a></li>
+                <li><a href="https://nationalinterest.org/blog/reboot/americas-sixth-generation-fighter-flying-we-know-nothing-else-180326">National Intrest Website</a></li>
+                <li><a href="https://www.msn.com/en-us/news/technology/has-the-air-force-already-shown-us-its-secret-new-fighter-jet/ar-BB19ljhv">MSN News</a></li>
+            </ul>
+        </p> 
+        </p>
         </div>
 
 
